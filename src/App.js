01831b9b7180ecf0787cc88596421fe8e6d3e0bc
AdenWhitworth <aden.whitworth@gmail.com>
2023-09-26 18:46:30 -0700

    Initialize React and Chessboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+/* Main App where UI will take place  */
 
-function App() {
+import React, {Component}  from 'react';
+import Chessboard from "chessboardjsx";
+import {Chess} from "chess.js";
+
+class GameLogic extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      fen: "start",
+      history: []
+    };
+  }
+
+  //when loading in create a new game
+  componentDidMount() {
+    this.game = new Chess();
+  };
+
+  //Logic performed on piece drop
+  onDrop = ({sourceSquare, targetSquare}) => {
+    let current_move = this.game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q"
+    })
+
+    if(current_move === null){
+      return;
+    }
+
+    this.setState(({ history, pieceSquare}) =>({
+      fen: this.game.fen(),
+      history: this.game.history({ verbose: true}),
+    }));
+    
+  };
+  
+
+  render() {
+
+    return this.props.children({
+      position: this.state.fen,
+      onDrop: this.onDrop
+    });
+
+  };
+  
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chess</h1>
+      <GameLogic>{({
+        position,
+        onDrop
+        }) => (
+          <Chessboard
+            width= {320}
+            id="chess_game" 
+            position={position}
+            onDrop={onDrop}
+            boardStyle={{
+              borderRadius: "5px",
+              boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+            }}
+          />
+        )}
+      </GameLogic>
     </div>
   );
 }
-
-export default App;
