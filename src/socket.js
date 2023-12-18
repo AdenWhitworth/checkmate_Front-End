905f9fd0f6a5498a3732a62e2e232c63fbe43107
AdenWhitworth <aden.whitworth@gmail.com>
2023-12-18 10:13:53 -0600

    added server url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io('localhost:8080'); // initialize websocket connection
+//const socket = io('localhost:8080'); // initialize websocket connection
+const socket = io('https://online-chess-with-friends-server.glitch.me');
 
 export default socket;
