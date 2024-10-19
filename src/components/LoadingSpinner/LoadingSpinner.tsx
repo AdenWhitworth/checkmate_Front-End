11297c93328a7b630f85762660c33dd0bae47de5
AdenWhitworth <aden.whitworth@gmail.com>
2024-10-19 18:49:40 -0400

    added jsdoc comments to the loading spinner

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./LoadingSpinner.css";
 
+/**
+ * LoadingSpinner Component displays a loading spinner to indicate ongoing processes or data loading.
+ * @component
+ * @returns {JSX.Element} The rendered loading spinner component.
+ */
+
 export default function LoadingSpinner(): JSX.Element {
     return (
         <div className="loading-container">
