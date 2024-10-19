c0c999126ace5d65aa437b6c4cc9e699d0a352a5
AdenWhitworth <aden.whitworth@gmail.com>
2024-10-19 19:00:48 -0400

    added jsdoc comments to the search bar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,6 +3,14 @@ import "./SearchBar.css";
 import { SearchBarProps } from './SearchBarTypes';
 import Search from "../../Images/Search.svg";
 
+/**
+ * SearchBar component that renders a styled search input field with a search icon.
+ *
+ * @component
+ * @param {SearchBarProps} props - The props for the SearchBar component.
+ * @param {function} props.onChange - Callback function that handles changes to the input field.
+ * @returns {JSX.Element} The rendered SearchBar component.
+ */
 export default function SearchBar({ onChange }: SearchBarProps): JSX.Element {
   return (
     <div className="search-bar">
