a47f1645df524d18dfeeb75588a6c75b3b4f3807
AdenWhitworth <aden.whitworth@gmail.com>
2024-10-19 18:47:56 -0400

    added jsdoc comments to loading dots component

--- src/components/LoadingDots/LoadingDots.tsx
+++ src/components/LoadingDots/LoadingDots.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import './LoadingDots.css';
 import { LoadingDotsProps } from './LoadingDotsTypes';
 
+/**
+ * LoadingDots component displays an animated series of dots indicating a loading state.
+ * The component is customizable in terms of position, color, and size using CSS classes.
+ * 
+ * @component
+ * @param {Object} props - Component props.
+ * @param {string} props.position - The position of the loading dots (e.g., "center", "left", "right").
+ * @param {string} props.color - The color class applied to the loading dots (e.g., "grey", "black").
+ * @param {string} props.size - The size class applied to the loading dots (e.g., "small", "medium").
+ * 
+ * @returns {JSX.Element} The rendered LoadingDots component.
+ */
+
 export default function LoadingDots({
     position,
     color,
     size
-}: LoadingDotsProps) {
+}: LoadingDotsProps): JSX.Element {
 
     return (
         <div className={`loading-dots position-${position}`} data-testid="loading-dots">
--- src/components/LoadingDots/LoadingDotsTypes.ts
+++ src/components/LoadingDots/LoadingDotsTypes.ts
@@ -1,3 +1,12 @@
+/**
+ * Props for the LoadingDots component to define its position, color, and size.
+ * 
+ * @interface LoadingDotsProps
+ * 
+ * @property {"left" | "center" | "right"} position - Defines the alignment of the loading dots within the container.
+ * @property {"grey" | "black"} color - Defines the color theme of the loading dots.
+ * @property {"small" | "medium" | "large"} size - Defines the size of each dot in the loading indicator.
+ */
 export interface LoadingDotsProps {
     position: "left" | "center" | "right";
     color: "grey" | "black";
