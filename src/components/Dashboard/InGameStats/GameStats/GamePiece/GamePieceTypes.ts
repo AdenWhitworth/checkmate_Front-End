566bd49271009ccb7cf55e15733ad15a4e46313a
AdenWhitworth <aden.whitworth@gmail.com>
2024-10-20 15:31:17 -0400

    added jsdoc comments to game piece types

--- src/components/Dashboard/InGameStats/GameStats/GamePiece/GamePieceTypes.ts
+++ src/components/Dashboard/InGameStats/GameStats/GamePiece/GamePieceTypes.ts
@@ -1,5 +1,13 @@
 import { PieceState } from '../../InGameStatsTypes';
 
+/**
+ * Interface representing the properties for the GamePiece component.
+ * 
+ * @interface GamePieceProps
+ * 
+ * @property {PieceState} piece - The state of the game piece, including its style and image details.
+ * @property {string} username - The username of the player who owns this game piece.
+ */
 export interface GamePieceProps {
     piece: PieceState;
     username: string;
